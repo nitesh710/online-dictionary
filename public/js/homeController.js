c170649bac0244f5ce402c96b1a4a478eba8f51f
nitesh710 <dehariyanitesh@gmail.com>
2018-05-12 15:24:24 -0700

    changes in footer.html and homeController.js

--- public/js/homeController.js
+++ public/js/homeController.js
@@ -1,33 +1,32 @@
 angular.module('homeController', [])
-.controller('homeCtrl', ['$scope','$http', function($scope, $http){
-	var data_json, words, def;
-	$scope.toggle = false;
-	$scope.notfound = false;
+    .controller('homeCtrl', ['$scope', '$http', function($scope, $http) {
+        var data_json, words, def;
+        $scope.toggle = false;
+        $scope.notfound = false;
 
-	$scope.searchWord = function(){
-		$http.get('data.json')
-		.then(function onSuccess(response){
-			console.log("successfully got it!");
-			data_json = JSON.parse(JSON.stringify(response.data));
+        $scope.searchWord = function() {
+            $http.get('data.json')
+                .then(function onSuccess(response) {
+                    console.log("successfully got it!");
+                    data_json = JSON.parse(JSON.stringify(response.data));
 
-			words = JSON.stringify($scope.word);
-			console.log(words);
+                    words = JSON.stringify($scope.word);
+                    console.log(words);
 
-			def = data_json[$scope.word];
+                    def = data_json[$scope.word];
 
-			if (def) {
-				$scope.toggle = true;
-				$scope.notfound = false;
-				$scope.content = def;
-			}
-			else{
-				$scope.toggle = false;
-				$scope.notfound = true;
-			}
-			
+                    if (def) {
+                        $scope.toggle = true;
+                        $scope.notfound = false;
+                        $scope.content = def;
+                    } else {
+                        $scope.toggle = false;
+                        $scope.notfound = true;
+                    }
 
-		}, function onError(response){
-			console.log("got error!", response);
-		});
-	};
-}]);
+
+                }, function onError(response) {
+                    console.log("got error!", response);
+                });
+        };
+    }]);
